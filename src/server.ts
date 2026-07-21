b88fd3eb804d4cf1150a33af1f043068abfd76d1
minhazchy101 <minhazchowdhury101@gmail.com>
2026-07-22 00:21:32 +0600

    feat: configure prisma connection & create user and profile model

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,21 @@
 import { app } from "./app"
 import config from "./config";
+import { prisma } from "./lib/prisma";
 
 
 const port = config.port;
 
 async function main() {
     try {
+         await prisma.$connect();
+          console.log("Prisma is running..");
+
         app.listen(port, ()=>{
        console.log(`Example app listening on port http://localhost:${port}`);
     })
 } catch (error) {
-        console.log(`Error from app ${error}`);
+    console.log(`Error from app ${error}`);
+    await prisma.$disconnect();
         process.exit(1)
     }
 }
